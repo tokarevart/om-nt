30bc0dbc9b99a18b96c2bdd22ce3aa8677e9027b
Tokarev Artyom <tokarev28.art@gmail.com>
2020-08-29 01:16:44 +0500

    upd

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,9 @@ pub use std::ops::Range;
 
 pub fn search(
     range: Range<f64>, init: f64, eps: f64,
-    der: impl Fn(f64) -> f64, der2: impl Fn(f64) -> f64) -> f64 {
-    
+    der: impl Fn(f64) -> f64, der2: impl Fn(f64) -> f64
+) -> f64 {
+
     assert!((range.start..=range.end).contains(&init));
     assert!(eps > 0.0);
     
@@ -19,7 +20,8 @@ pub fn search(
 
 pub fn search_with_n(
     range: Range<f64>, init: f64, n: usize,
-    der: impl Fn(f64) -> f64, der2: impl Fn(f64) -> f64) -> f64 {
+    der: impl Fn(f64) -> f64, der2: impl Fn(f64) -> f64
+) -> f64 {
     
     assert!((range.start..=range.end).contains(&init));
     assert!(n > 0);
